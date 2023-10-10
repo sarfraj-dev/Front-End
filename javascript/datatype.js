6857599f2f575c570109782663bfb2754b0ab359
sarfraj sayyad <sarfraj1711@gmail.com>
2023-10-11 00:54:47 +0530

    different data type in js

---
+++ javascript/datatype.js
@@ -0,0 +1,18 @@
+"use strict"; //treat all js code as newer version of js
+
+//alert(3+3); // we are using node js not browser
+
+/*
+number
+bigint
+string
+boolearn
+null => standalone value
+undefined =>
+symbol =>unique
+*/
+
+//object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
