398aa4bac617b72917b99f65f92f739d5d3af7a6
sarfraj sayyad <sarfraj1711@gmail.com>
2023-10-11 23:31:34 +0530

    arithmetic operators

---
+++ javascript/operation.js
@@ -0,0 +1,14 @@
+let value=17;
+let negValue=-value;
+console.log(negValue);
+
+let str1="hello ";
+let str2="sarfraj";
+let str3= str1+ str2;
+console.log(str3);
+
+console.log("1"+2);
+console.log(1+"2");
+console.log(1+"2"+2);  //122
+console.log(1+2+"2");  //32
+
