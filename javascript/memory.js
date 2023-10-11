409f2a725ff924170684048ee52f9d854b334c23
sarfraj sayyad <sarfraj1711@gmail.com>
2023-10-12 00:39:25 +0530

    stack memory and heap memory how it works

---
+++ javascript/memory.js
@@ -0,0 +1,25 @@
+//  Stack Memory ( inside primitive data type stack memory use)
+
+
+
+let name="sarfraj";
+let lastName=name;
+lastName="sayyad";
+
+console.log(name); // sarfraj
+console.log(lastName);  //sayyad
+
+
+//  heap Memory ( inside non-primitive data type stack memory use)
+
+// it takes the same reference and change the original value
+let userOne={
+    name:"sarfraj",
+    age:22
+}
+console.log(userOne.age);
+let userTwo= userOne;
+console.log(userTwo.age);
+userTwo.age=24;
+console.log(userTwo.age);
+console.log(userOne.age);
