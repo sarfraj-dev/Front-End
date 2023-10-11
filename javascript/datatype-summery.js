8a114730a3817cadd446dbb64aeb3a3157702238
sarfraj sayyad <sarfraj1711@gmail.com>
2023-10-12 00:08:10 +0530

    datatype-summery

---
+++ javascript/datatype-summery.js
@@ -0,0 +1,23 @@
+// Javascript is dynamically typed language (no need to define type like string number and all)
+
+// Primitive
+/*
+String
+Number
+Bollean
+null
+undefiner
+symbol
+BigInt
+*/
+let id = Symbol (1711);
+let uniqueId= Symbol(1711);
+console.log(id===uniqueId);   //symbol are unique => false
+
+//Non Primitive
+
+/*
+Array
+Object
+Functions
+*/
