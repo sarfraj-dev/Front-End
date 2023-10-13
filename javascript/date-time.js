9ab7ff7c417ddc26718edd513179ee9a87445a75
sarfraj sayyad <sarfraj1711@gmail.com>
2023-10-13 23:39:05 +0530

    date and time

---
+++ javascript/date-time.js
@@ -0,0 +1,19 @@
+//  date is object
+
+// Date
+
+
+let myDate = new Date()
+console.log(myDate.toString());
+
+// different type of format to write a date
+
+// let creatDate= new Date(2023,0,20)
+// let creatDate= new Date(2023,0,20,5,8)  // here month start from 0
+let creatDate= new Date("2001-11-17") // if you want to define specific date in this senario month start from 0
+console.log(creatDate.toString());
+
+//  Time
+/*
+where we need time :-  when you build quiz app or poll to find time
+/*
