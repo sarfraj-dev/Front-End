e6103665ac66199554fe6fd0194d950d3279fb65
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2023-10-16 01:05:23 +0530

    how to use spread operator and hasOwnProperty method

--- javascript/object.js
+++ javascript/object.js
@@ -9,9 +9,10 @@ Note : - array is non primitive data type and use heap memory so it change the o
 
 
 //interview question create a symbol add into object and print the value
-const addSymbol= Symbol()
+// const addSymbol= Symbol()
 
 //object literals
+/*
 const jsUser={
     name:"sarfraj",
     [addSymbol]:"interview", //how to add symbol interview question into object if you add like addSymbol:"interview", in console it prints the value only but if you check the type of that time it shows string but we want datatype as symbol only
@@ -21,7 +22,7 @@ const jsUser={
     email:"sarfraj@google.com",
     lastLogin:["monday", "friday"]
 }
-
+*/
 /*
 // interview console output
 console.log(typeof(jsUser.addSymbol)); //here the type of addsymbol is string but we want as symbol only so
@@ -63,4 +64,80 @@ console.log(jsUser.grettingSecond());
 console.log("B",jsUser);
 console.log(jsUser);
 
+*/
+/*
+// how to access same object referemce using this
+jsUser.whishing=function(){
+    console.log(`hello ${this.name}`);
+}
+console.log(jsUser.whishing());
+*/
+
+// how to define singleton object 
+// const tinderUser=new Object()
+
+/* how to add element in object
+const swiggyUser={}
+// console.log(swiggyUser);
+
+swiggyUser.name="rakesh"
+swiggyUser.age=22
+swiggyUser.id="17feb"
+console.log(swiggyUser);
+*/
+
+/*
+// object nesting
+const regularUser={
+    email:"rajesh@gmail.com",
+    fullname:{
+        userfullname:{
+            firstname:"rajesh",
+        }
+    }
+}
+console.log(regularUser.fullname.userfullname.firstname);
+*/
+
+/*
+// merge two object
+/*
+const obj1={1:"a",2:"b",3:"c"}
+const obj2={4:"d",5:"e",6:"f"}
+
+//  to merge object use spread operator
+const obj3={...obj1,...obj2}
+console.log(obj3);
+*/
+
+/*
+not use this process methos because spread operator you can use
+
+// const obj3={obj1, obj2}
+const obj3=Object.assign({},obj1,obj2) // empty {} is optional but use when you check the obj3=== obj1 output false and value not store in obj1 it store in empty object
+console.log(obj3===obj1);
+console.log(obj3);
+console.log(obj1);
+
+
+const obj4=Object.assign(obj1,obj2) // if you are not using empty object all value store in obj1 so when you check equality or obj1 object it shows true because all value store in obj1
+console.log(obj4===obj1);
+console.log(obj1);
+*/
+
+
+/*
+Important all value and values of object store in array 
+*/
+/*
+// mostly use in project because you can loop 
+const obj5={1:"a",2:"b",3:"c"}
+console.log(Object.keys(obj5)); // all key value store in array
+console.log(Object.values(obj5)); // all object value store in array
+*/
+
+/*
+// if you want to check whether property is exist in object or not
+const obj7={1:"a",2:"b",3:"c"}
+console.log(obj7.hasOwnProperty(age)); // it return boolean value
 */
