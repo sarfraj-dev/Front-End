cf26e6378ff95ff35cb9e0d9b7320cff365188fb
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2023-10-21 23:52:08 +0530

    how javascript works

---
+++ javascript/how-js-works.js
@@ -0,0 +1,9 @@
+/*
+how js file runs
+
+1 global execution conterxt created and this variable assigned
+2 memory phase : - it assigns memoray to variable and function
+3 execution phase :- it execute the code
+*/
+
+// call stack
