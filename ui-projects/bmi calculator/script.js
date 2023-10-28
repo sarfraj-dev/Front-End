38a85214f7e6056cb23953495c9d684a7791f182
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2023-10-29 01:42:03 +0530

    basic setup of bmi project

---
+++ ui-projects/bmi calculator/script.js
@@ -0,0 +1,9 @@
+const height=document.querySelector('#height')
+const weight=document.querySelector('#weight')
+const result=document.querySelector('#result')
+const button=document.querySelector('.button')
+
+button.addEventListener('click', function(e){
+    e.preventDefault()
+    he
+})
