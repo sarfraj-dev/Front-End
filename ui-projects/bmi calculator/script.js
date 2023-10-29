088c83d5323f3af2ff31121cacfc9d35bb42df9d
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2023-10-29 11:39:05 +0530

    bmi calculator

--- ui-projects/bmi calculator/script.js
+++ ui-projects/bmi calculator/script.js
@@ -1,9 +1,28 @@
-const height=document.querySelector('#height')
-const weight=document.querySelector('#weight')
-const result=document.querySelector('#result')
-const button=document.querySelector('.button')
+/*
+1 why we use all selector inside function because we want the value if we use outside it store empty value
+2 why we use preventdefault() because when we click on sumbit value goes to store in server we have to cancel the behaviour of form 
+*/
 
-button.addEventListener('click', function(e){
+const form=document.querySelector('form')
+
+form.addEventListener('submit', function(e){
     e.preventDefault()
-    he
-})
+    const height=parseInt(document.querySelector('#height').value)
+const weight=parseInt(document.querySelector('#weight').value)
+const result=document.querySelector('#result')
+
+
+if(height==='' || height <=0 || isNaN(height)){
+result.innerHTML="please enter a valid number"
+}
+else if(weight==='' || weight <=0 || isNaN(weight)){
+result.innerHTML="please "
+}
+else{
+    const formula=(weight/((height*height)/10000)).toFixed(2)
+    result.innerHTML=formula
+}
+
+
+    })
+
