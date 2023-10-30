b1b5fc9ebf3da35256ffe12919d381d0f8d0d1ab
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2023-10-31 01:39:12 +0530

    unlimited colors generator

---
+++ ui-projects/unlimited colors/script.js
@@ -0,0 +1,35 @@
+const start=document.querySelector("#start")
+const body=document.querySelector('body')
+
+const stop=document.querySelector("#stop")
+
+const randomColors=()=>{
+    const values="0123456789ABCDEF"
+    let color="#"
+    for (let i = 0; i < 6; i++) {
+      color +=  values[Math.floor(Math.random()*16)]
+    }
+    // console.log(color);
+return color
+}
+
+const changeBgColor=()=>{
+    body.style.background=randomColors()
+}
+let setInt
+console.log(setInt);
+
+const changingColor=()=>{
+    setInt =setInterval(changeBgColor,1000);
+}
+   const stopchanging=()=>{
+    clearInterval(setInt)
+    console.log(setInt);
+
+}
+console.log(setInt);
+
+stop.addEventListener('click',stopchanging)
+
+start.addEventListener('click',changingColor)
+
