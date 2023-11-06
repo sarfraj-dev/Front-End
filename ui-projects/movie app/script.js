f0709bd50ff398b8df00eb1eed8880ed098d2478
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2023-11-07 01:16:12 +0530

    display image and other data using api

---
+++ ui-projects/movie app/script.js
@@ -0,0 +1,47 @@
+const productTitle=document.querySelectorAll('.card-title')
+const cardText=document.querySelectorAll('.card-text')
+const img=document.querySelectorAll('.card-img-top')
+const imgBaseURL = 'https://image.tmdb.org/t/p/w500';
+let imgUL=document.querySelector('.imgUL')
+fetch('https://api.themoviedb.org/3/movie/popular?api_key=4e951421deea548f2910a28367a65366')
+.then((response)=>{
+    return response.json()
+}).then((data)=>{
+    console.log(data);
+return data.results
+
+})
+.then((responseData)=>{
+    responseData.forEach((item,index) => {
+        const value=item.title;
+         if(index<productTitle.length){
+    productTitle[index].innerHTML=value;
+
+        }
+});
+console.log(responseData);
+responseData.forEach((item,index)=>{
+    // console.log(item.overview)
+    const overview=item.overview
+    if (index<cardText.length) {
+    cardText[index].innerHTML=overview
+    }
+})
+responseData.forEach((item,index)=>{
+    
+    if (index<img.length) {
+    img[index].src=imgBaseURL + item.poster_path;
+    }
+})
+responseData.forEach((item)=>{
+    const cardItem= document.createElement('li')
+   cardItem.setAttribute("class", "li");
+    const imgElement=document.createElement('img')
+    imgElement.src=imgBaseURL+item.poster_path
+    cardItem.appendChild(imgElement)
+imgUL.appendChild(cardItem)
+// .src=imgBaseURL+item.poster_path
+
+
+})
+})
