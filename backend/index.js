4f60b87bb830692f043effec12e2de1d8a70184b
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2024-03-21 00:15:12 +0530

    how to use readfile asynchornosly

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,14 @@
-import {sum} from './lib.js'
-console.log(sum(4,5));
+// import {sum} from './lib.js'
+const { isUtf8 } = require('buffer');
+const fs=require('fs');
+// const txt=fs.readFileSync('demo.txt', "Utf-8")
+const t1=performance.now();
+fs.readFile('demo.txt', "Utf-8",(err,txt)=>{
+    console.log(txt);
+})
+const t2=performance.now();
+console.log(t2-t1);
+
+// console.log(txt);
+// console.log(sum(4,5));
 const a=5;
