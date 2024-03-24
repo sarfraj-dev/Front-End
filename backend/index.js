409c60faaf866a8b059f68b32103ed67cc7d97c0
sarfraj sayyad <sarfrajsayyad.dev@gmail.com>
2024-03-24 06:59:17 +0530

    dummy server created

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,8 @@
-// import {sum} from './lib.js'
-const { isUtf8 } = require('buffer');
-const fs=require('fs');
+const http=require("http");
 
-
-// const txt=fs.readFileSync('demo.txt', "Utf-8")
-const t1=performance.now();
-fs.readFile('demo.txt', "Utf-8",(err,txt)=>{
-    console.log(txt);
+const server=http.createServer((req,res)=>{
+console.log("server start");
+res.setHeader('Dummy','value')
+res.end('<h1>hello</h1>')
 })
-const t2=performance.now();
-console.log(t2-t1);
-
-// console.log(txt);
-// console.log(sum(4,5));
-const a=5;
+server.listen(8080)
